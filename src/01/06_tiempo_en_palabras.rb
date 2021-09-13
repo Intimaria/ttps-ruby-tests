f1d64ca8f0c9ff108317d618c8a5fbc66144725b
Intimaria <intimaria@gmail.com>
2021-09-13 00:02:18 -0300

    fix for tiempo en palabras 24hs & future logic

--- src/01/06_tiempo_en_palabras.rb
+++ src/01/06_tiempo_en_palabras.rb
@@ -3,9 +3,11 @@ require 'time'
 def formato(casi, futuro, minutos, tiempo)
   str = ""
   str << "casi " if casi
-  str << (tiempo.strftime("%l").to_i == 1 ? "es la " : "son las ")
-  str << (futuro ? (tiempo.strftime("%l").to_i + 1).to_s : tiempo.strftime("%l").to_i.to_s)
-  str << minutos
+  hora = casi || futuro ? (tiempo.hour + 1) : tiempo.hour
+  hora -= 12 if tiempo.strftime("%p") == "PM"
+  hora = 0 if (tiempo.strftime("%p") == "PM") & (hora == 12)
+  str << (hora == 1 ? "es la " : "son las ")
+  str << hora.to_s << minutos
   str.capitalize
 end
 
